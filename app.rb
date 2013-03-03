ee790dd2b676c340076e40221d36a735369f086c
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2013-03-03 14:30:16 -0500

    Use a before filter for error checking in 1 place.

--- app.rb
+++ app.rb
@@ -30,66 +30,58 @@ class App < Sinatra::Base
     end
   end
 
-  get "/:db/bad_queries" do
+  before "/:db/*" do
     halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
+  end
+
+  get "/:db/bad_queries" do
     exec_query(Queries::BAD_QUERIES, params[:db]).to_json
   end
 
   get "/:db/queries_tmp_tables" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::QUERIES_TMP_TABLES, params[:db]).to_json
   end
 
   get "/:db/table_stats/:schema_name/:table_name" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::TABLE_STATS, params[:db], params[:schema_name], params[:table_name]).to_json
   end
 
   get "/:db/index_stats/:schema_name/:table_name" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::INDEX_STATS, params[:db], params[:schema_name], params[:table_name]).to_json
   end
 
   get "/:db/schema_size/:schema_name" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::SCHEMA_SIZE, params[:db], params[:schema_name]).to_json
   end
 
   put "/:db/:username/lock" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::LOCK_USER, params[:db], params[:username])
     { "username" => params[:username], "status" => "locked" }.to_json
   end
 
   put "/:db/:username/unlock" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::UNLOCK_USER, params[:db], params[:username])
     { "username" => params[:username], "status" => "unlocked" }.to_json
   end
 
   get "/:db/trx_summary" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::TRX_SUMMARY, params[:db]).to_json
   end
 
   get "/:db/active_trx" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::ACTIVE_TRX, params[:db]).to_json
   end
 
   get "/:db/blocked_trx" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::BLOCKED_TRX, params[:db]).to_json
   end
 
   put "/:db/kill_idle_trx/:idle_time" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::KILL_IDLE_TRX, params[:db], params[:idle_time])
     { "status" => "ok" }.to_json
   end
 
   put "/:db/kill_blocking_trx/:idle_time" do
-    halt 422, { :message => "Invalid Database Host" }.to_json unless valid_db(params[:db])
     exec_query(Queries::KILL_BLOCKING_TRX, params[:db], params[:idle_time])
     { "status" => "ok" }.to_json
   end
